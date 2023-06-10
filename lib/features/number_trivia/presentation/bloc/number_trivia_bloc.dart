6430591f3d3af2b536e72ac0ff97abf22d7edea5
Endrju00 <75850182+Endrju00@users.noreply.github.com>
2023-06-10 16:48:37 +0200

    Implement NumberTriviaBloc

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/error/failures.dart';
+import '../../../../core/usecases/usecase.dart';
 import '../../../../core/util/input_converter.dart';
 import '../../domain/entities/number_trivia.dart';
 import '../../domain/usecases/get_concrete_number_trivia.dart';
@@ -33,9 +35,39 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
 
         inputEither.fold(
           (failure) => emit(const Error(message: invalidInputFailureMessage)),
-          (success) => null,
+          (integer) {
+            emit(Loading());
+            getConcreteNumberTrivia(
+              Params(number: integer),
+            ).then((failureOrTrivia) {
+              failureOrTrivia.fold(
+                (failure) => emit(Error(message: _mapFailureToMessage(failure))),
+                (trivia) => emit(Loaded(trivia: trivia)),
+              );
+            });
+          },
         );
       }
+      if (event is GetTriviaForRandomNumber) {
+        emit(Loading());
+        getRandomNumberTrivia(NoParams()).then((failureOrTrivia) {
+          failureOrTrivia.fold(
+            (failure) => emit(Error(message: _mapFailureToMessage(failure))),
+            (trivia) => emit(Loaded(trivia: trivia)),
+          );
+        });
+      }
     });
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }
